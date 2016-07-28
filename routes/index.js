5547c6364643eb6b48f4ef5bd395ffcf269a163a
locphan <phanhoangloc@gmail.com>
2016-07-28 09:14:10 +0700

    add example code for module sction

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
 module.exports = function () {
   console.log('routes index', __dirname);
+  console.log(require.main);
+  console.log(module);
 };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var routes = require('./routes')();
-
+console.log(require.main);
+console.log(module);
 console.log(process.cwd());
 console.log(__dirname);
